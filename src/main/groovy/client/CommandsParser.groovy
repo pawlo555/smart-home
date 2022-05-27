ccc69044e5ee502b9278bf66ca680319d9927e16
pawlo555 <spawel555@outlook.com>
2022-05-27 20:24:16 +0200

    - parsing commands

--- src/main/groovy/client/CommandsParser.groovy
+++ src/main/groovy/client/CommandsParser.groovy
@@ -1,5 +1,8 @@
 package client
 
+import SmartHome.Modes
+import SmartHome.PhotoSize
+import SmartHome.Time
 import pl.edu.agh.Commands
 
 class CommandsParser {
@@ -7,20 +10,76 @@ class CommandsParser {
     static parse(String line) {
         line = line.strip()
         def commands = line.split(" ")
-        if (isSpecialCommand(commands)) {
+        if (commands.size() == 1) {
             return [commands[0]]
         }
-        String device = "Janusz"
-        Commands command = Commands.valueOf("TURN_ON")
-        def param = getParam('Janusz')
-        return [device, command, param]
+        else {
+            if (commands.size() > 3) {
+                throw new IllegalArgumentException("Too many arguments in: ${line}")
+            }
+            String device = commands[0]
+            Commands command = Commands.valueOf(commands[1])
+            if (commands.size() == 3) {
+                def param = getParam(commands[2], command)
+                return [device, command, param]
+            }
+            return [device, command ]
+        }
     }
 
-    static boolean isSpecialCommand(String[] commands) {
-        return commands.size() == 1
+    def static getParam(String name, Commands command) {
+        switch(command) {
+            case Commands.SET_MODE:
+                return parseMode(name)
+            case Commands.SET_PHOTO_SIZE:
+                return parsePhotoSize(name)
+            case Commands.SET_TEMPERATURE:
+                return parseTemperature(name)
+            case Commands.SET_TIME:
+                return parseTime(name)
+        }
     }
 
-    def static getParam(String name) {
+    def static parseMode(String name) {
+        try {
+            return Modes.valueOf(name)
+        }
+        catch (Exception ignored) {
+            throw new IllegalArgumentException("Invalid oven mode - available modes:\n ${Modes.values()}")
+        }
+    }
 
+    def static parsePhotoSize(String size) {
+        try {
+            def list = size.split(":")
+            short x = Short.parseShort(list[0])
+            short y = Short.parseShort(list[1])
+            return new PhotoSize(x,y)
+        }
+        catch (Exception ignored) {
+            throw new IllegalArgumentException("Invalid photoSize: ${size} - format is: x:y")
+        }
+    }
+
+    def static parseTemperature(String temp) {
+        try {
+            return Short.parseShort(temp)
+        }
+        catch (Exception ignored) {
+            throw new IllegalArgumentException("Invalid temperature: ${temp} - format is: x")
+        }
+    }
+
+    def static parseTime(String timeName) {
+        try {
+            def list = timeName.split(":")
+            short seconds = Short.parseShort(list[2])
+            short minutes = Short.parseShort(list[1])
+            short hours = Short.parseShort(list[0])
+            return new Time(seconds, minutes, hours)
+        }
+        catch (Exception ignored) {
+            throw new IllegalArgumentException("Invalid time: ${timeName} - format is: hours:minutes:seconds}")
+        }
     }
 }
--- src/test/test_groovy/ParserTests.groovy
+++ src/test/test_groovy/ParserTests.groovy
@@ -1,8 +1,12 @@
 package test_groovy
 
+import SmartHome.Modes
+import SmartHome.Time
 import client.CommandsParser
 import org.junit.Test
 import pl.edu.agh.Commands
+import SmartHome.PhotoSize
+
 
 import static org.junit.Assert.assertEquals
 
@@ -29,18 +33,88 @@ class ParserTests {
     @Test
     void turnOnTest() {
         def output = CommandsParser.parse("Device1 TURN_ON")
-        assertEquals(["Device1", Commands.TURN_ON, null], output)
+        assertEquals(["Device1", Commands.TURN_ON], output)
     }
 
     @Test
     void turnOffTest() {
         def output = CommandsParser.parse("LightBulb1 TURN_OFF ")
-        assertEquals(["LightBulb1", Commands.TURN_OFF, null], output)
+        assertEquals(["LightBulb1", Commands.TURN_OFF], output)
     }
 
     @Test
     void checkPowerTest() {
         def output = CommandsParser.parse("LightBulb1 CHECK_POWER ")
-        assertEquals(["LightBulb1", Commands.CHECK_POWER, null], output)
+        assertEquals(["LightBulb1", Commands.CHECK_POWER], output)
+    }
+
+    @Test
+    void checkGetHumidityTest() {
+        def output = CommandsParser.parse("MultiFunctionalOven1 GET_HUMIDITY ")
+        assertEquals(["MultiFunctionalOven1", Commands.GET_HUMIDITY], output)
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    void checkInvalidCommandTest() {
+        CommandsParser.parse("SomeDevice INVALID_COMMAND")
+    }
+
+    @Test()
+    void checkSetModeParseTest() {
+        def output = CommandsParser.parse("MultiFunctionalOven1 SET_MODE ECO")
+        assertEquals(["MultiFunctionalOven1", Commands.SET_MODE, Modes.ECO], output)
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    void checkInvalidSetModeParseTest() {
+        CommandsParser.parse("MultiFunctionalOven1 SET_MODE ECO2.0")
+    }
+
+    @Test()
+    void checkSetPhotoSizeParseTest() {
+        def output = CommandsParser.parse("MultiFunctionalOven1 SET_PHOTO_SIZE 10:20")
+        assertEquals(["MultiFunctionalOven1", Commands.SET_PHOTO_SIZE, new PhotoSize(10 as short, 20 as short)], output)
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    void checkInvalidSetPhotoSizeParseTest() {
+        CommandsParser.parse("Camera SET_PHOTO_SIZE 134")
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    void checkInvalidStringSetPhotoSizeParseTest() {
+        CommandsParser.parse("Camera SET_PHOTO_SIZE cos:cos")
+    }
+
+    @Test()
+    void checkSetTemperatureParseTest() {
+        def output = CommandsParser.parse("Oven1 SET_TEMPERATURE 213")
+        assertEquals(["Oven1", Commands.SET_TEMPERATURE, 213 as short], output)
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    void checkInvalidSetTemperatureParseTest() {
+        CommandsParser.parse("Oven1 SET_TEMPERATURE hundred")
+    }
+
+    @Test()
+    void checkTimeParseTest() {
+        def output = CommandsParser.parse("Oven1 SET_TIME 4:23:30")
+        assertEquals(["Oven1", Commands.SET_TIME, new Time(30 as short, 23 as short, 4 as short)], output)
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    void checkInvalidTimeParseTest() {
+        CommandsParser.parse("Oven1 SET_TIME ref:23:30")
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    void checkInvalidStringTimeParseTest() {
+        CommandsParser.parse("Oven1 SET_TIME oneHour")
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    void fourStringParseTest() {
+        CommandsParser.parse("Oven1 SET_TIME oneHour dwa")
     }
 }
